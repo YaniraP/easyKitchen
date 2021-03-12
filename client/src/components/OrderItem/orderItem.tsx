ebf6263bad5cbd2091a31e77a65edfef526ac074
Mathias Soderqvist <mathias.soderqvist@live.com>
2021-03-12 20:21:05 +0100

    refactor(OrderItem): orderItem TS refactor

---
+++ client/src/components/OrderItem/orderItem.tsx
@@ -0,0 +1,27 @@
+import './orderItem.css'
+import { Order } from '../../interfaces/order';
+
+interface OrderItemProps {
+  order: Order
+}
+
+const OrderItem: React.FC<OrderItemProps> = ({ order })  => {
+
+  return (
+    <div className='orderItem'>
+      <p>Order ID: {order.id}</p>
+      <p>Name: {order.clientName}</p>
+      <p>Address:  {order.clientAddress}</p>
+      <p>Phone: {order.clientPhone}</p>
+      <p>Comments: {order.comments}</p>
+
+      {order.Dishes.map((dish) =>
+        <div key={dish.id}>
+          <p >{dish.title}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default OrderItem;
