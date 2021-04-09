ebe745a502992f5f9f5a0c65a922f77c27ca95a4
yanira <yanira.p@hotmail.com>
2021-04-09 18:48:03 +0200

    fix: remove hard coded server port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const db = require('./models')
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(morgan('tiny'))
 app.use(cors())
